897e6b579f6934e9d8bb5aef3bd7cbe01e3520ea
Brad Geesaman <3769609+bgeesaman@users.noreply.github.com>
2024-09-10 18:00:18 -0400

    versioning lifecycle

--- terraform/modules/caizen_core/storage.tf
+++ terraform/modules/caizen_core/storage.tf
@@ -15,26 +15,39 @@ resource "google_storage_bucket" "data_ingest" {
     enabled = true
   }
 
-  // Move to next storage class after N days
+  // Move to next storage class if not the latest version
   lifecycle_rule {
     action {
       type          = "SetStorageClass"
       storage_class = var.ingest_next_storage_class
     }
     condition {
-      age = var.ingest_next_storage_class_age_days
+      num_newer_versions = 1
+      with_state         = "ARCHIVED"
     }
   }
 
-  // Delete all objects after N days
+  // Delete all objects after N days if they aren't the latest version
   lifecycle_rule {
     action {
       type = "Delete"
     }
 
     condition {
-      age        = var.ingest_bucket_name_delete_age_days
-      with_state = "ANY"
+      age                = var.ingest_bucket_name_delete_age_days
+      num_newer_versions = 2
+      with_state         = "ARCHIVED"
+    }
+  }
+
+  // Delete all objects after a year regardless
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 365
     }
   }
 
